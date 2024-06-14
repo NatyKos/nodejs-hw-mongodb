9a74f66f7dc08df168917f05082b7d4d55cd8147
NatyKos <145201378+NatyKos@users.noreply.github.com>
2024-06-14 16:58:50 +0100

    done

--- src/server.js
+++ src/server.js
@@ -9,45 +9,68 @@ const PORT = Number(env('PORT', '3000'));
 const setupServer = () => {
   const logger = pino({ transport: { target: 'pino-pretty' } });
   const app = express();
-
   app.use(logger);
   app.use(cors());
 
   app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  });
-
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    const contact = await getContactById(contactId);
-    // Відповідь, якщо контакт не знайдено
-    if (!contact) {
-      res.status(404).json({
-        status: 404,
-        message: 'Contact not found',
+    try {
+      const allContacts = await getAllContacts();
+      res.status(200).json({
+        status: 200,
+        message: 'Successfully found contacts!',
+        data: allContacts,
       });
-      return;
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Oops, something went wrong',
+        error: error.message,
+      });
     }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-      data: contact,
-    });
-    next();
   });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contactById = await getContactById(contactId);
+
+      // Відповідь, якщо контакт не знайдено
+      if (!contactById) {
+        res.status(404).json({
+          status: 404,
+          message: 'Contact not found',
+        });
+      }
+      // Відповідь, якщо контакт знайдено
+      res.status(200).json({
+        status: 200,
+        message: `Successfully found contact with id ${contactId}!`,
+        data: contactById,
+      });
+    } catch (error) {
+      res.status(500).json({
+        status: 500,
+        message: 'Oops, something went wrong',
+        error: error.message,
+      });
+    }
+  });
+
   app.use('*', (req, res) => {
     res.status(404).json({
       status: 404,
       message: 'Not found',
     });
   });
+
+  app.use((error, req, res) => {
+    res.status(500).json({
+      status: 500,
+      message: 'Something went wrong',
+      error: error.message,
+    });
+  });
+
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
